96ed61a097159c4b39113bb7acece89268958daa
JuniorM <jsammartins09@icloud.com>
2026-07-10 23:37:06 -0400

    Dict^2

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-#dictionary
-dictionary = {"one" : 1 , "two" : 2 , "three" : 3 }
-print(dictionary["one"])
+list = [1 , 2 , 3 , 4 , 5 , 6]
+var1 = len(list)
+list.append(7)
+var2 = len(list)
+print(var2 - var1)
 
-database = {"key1" : 12345 , "key2" : 54321}
-print("key1")
-
+list1 = {"zero" : 1 , "one" : 2 , "two" : 3 , "three" : 4 , "four" : 5 , "five" : 6}
+print(list1["three"] * list1["five"])
+list1["six"] = 7
+print(list1)
