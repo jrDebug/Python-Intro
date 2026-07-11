6e1d428d6a3a24154e77fdd0427ffbabf2292596
JuniorM <jsammartins09@icloud.com>
2026-07-10 23:54:25 -0400

    for

--- main.py
+++ main.py
@@ -1,31 +1,5 @@
-big = 4
-
-if big == 2:
-    print("unserious")
-elif big == 1:
-    print("unserious")
-elif big == 3:
-    print("unserious")
-else:
-    print("locked")
-
-v1 = 2
-v2 = 2
-if v1 != v2:
-    print("unequal")
-else:
-    print("equal")
-
-score = 15
-
-if score >= 90:
-    print ("A")
-elif score >= 80:
-    print ("B")
-elif score >= 70:
-    print ("C")
-elif score >= 60:
-    print ("D")
-else:
-    print ("F")
-    
+fruits = ("apple" , "banana" , "watermelon")
+for x in fruits:
+    if x != "banana":
+        print(x)
+        
